944e98e2c3564bb18338adf2da460ef7c1febb87
qscrim <tochnonemaltsev@yandex.ru>
2026-04-08 00:52:54 +0700

    Coroutines: Реализация SeekBar и подсчёт корутин

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.coroutinedemo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.coroutinedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupSeekBar()
+    }
+
+    private fun setupSeekBar() {
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                count = if (progress == 0) 1 else progress
+                binding.countText.text = "${count} coroutines"
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // Не используется
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // Не используется
+            }
+        })
     }
 }
